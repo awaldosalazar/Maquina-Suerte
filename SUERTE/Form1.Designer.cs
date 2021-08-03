a59058b8a49ea8950d816f4396ec78df9d66eefa
awaldosalazar <73403289+awaldosalazar@users.noreply.github.com>
2021-08-03 09:53:10 -0500

    subiendo proyecto

---
+++ SUERTE/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SUERTE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelPrimero = new System.Windows.Forms.Label();
+            this.labelSegundo = new System.Windows.Forms.Label();
+            this.labelSigno = new System.Windows.Forms.Label();
+            this.textBoxResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(210, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 60);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelPrimero
+            // 
+            this.labelPrimero.AutoSize = true;
+            this.labelPrimero.BackColor = System.Drawing.Color.Transparent;
+            this.labelPrimero.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrimero.Location = new System.Drawing.Point(341, 88);
+            this.labelPrimero.Name = "labelPrimero";
+            this.labelPrimero.Size = new System.Drawing.Size(99, 76);
+            this.labelPrimero.TabIndex = 1;
+            this.labelPrimero.Text = "n°";
+            this.labelPrimero.Visible = false;
+            // 
+            // labelSegundo
+            // 
+            this.labelSegundo.AutoSize = true;
+            this.labelSegundo.BackColor = System.Drawing.Color.Transparent;
+            this.labelSegundo.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSegundo.Location = new System.Drawing.Point(215, 88);
+            this.labelSegundo.Name = "labelSegundo";
+            this.labelSegundo.Size = new System.Drawing.Size(99, 76);
+            this.labelSegundo.TabIndex = 2;
+            this.labelSegundo.Text = "n°";
+            this.labelSegundo.Visible = false;
+            // 
+            // labelSigno
+            // 
+            this.labelSigno.AutoSize = true;
+            this.labelSigno.BackColor = System.Drawing.Color.Transparent;
+            this.labelSigno.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSigno.Location = new System.Drawing.Point(74, 85);
+            this.labelSigno.Name = "labelSigno";
+            this.labelSigno.Size = new System.Drawing.Size(116, 76);
+            this.labelSigno.TabIndex = 3;
+            this.labelSigno.Text = "+/-";
+            this.labelSigno.Visible = false;
+            // 
+            // textBoxResultado
+            // 
+            this.textBoxResultado.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBoxResultado.Location = new System.Drawing.Point(176, 38);
+            this.textBoxResultado.Multiline = true;
+            this.textBoxResultado.Name = "textBoxResultado";
+            this.textBoxResultado.Size = new System.Drawing.Size(148, 31);
+            this.textBoxResultado.TabIndex = 4;
+            this.textBoxResultado.Text = "TU ULTIMO RESULTADO ES:\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(500, 249);
+            this.Controls.Add(this.textBoxResultado);
+            this.Controls.Add(this.labelSigno);
+            this.Controls.Add(this.labelSegundo);
+            this.Controls.Add(this.labelPrimero);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelPrimero;
+        private System.Windows.Forms.Label labelSegundo;
+        private System.Windows.Forms.Label labelSigno;
+        private System.Windows.Forms.TextBox textBoxResultado;
+    }
+}
+
---
+++ SUERTE/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SUERTE
+{
+    public partial class Form1 : Form
+    {
+        int primero, segundo, tercero;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void primerosacar()
+        {
+            Random primernumero = new Random();
+            primero = primernumero.Next(0, 10);
+        }
+        public void segundosacar()
+        {
+            Random segundonumero = new Random();
+            segundo = segundonumero.Next(0, 2);
+        }
+        public void tercerosacar()
+        {
+            Random terceronumero = new Random();
+            tercero = terceronumero.Next(0, 100);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            textBoxResultado.Visible = false;
+            labelPrimero.Visible = true;
+            labelSegundo.Visible = true;
+            labelSigno.Visible = true;
+            primerosacar();
+            segundosacar();
+            tercerosacar();
+            animacionGeneral();
+            button1.Visible = true;
+            textBoxResultado.Visible = true;
+            labelPrimero.Visible = false;
+            labelSegundo.Visible = false;
+            labelSigno.Visible = false;
+            if ((tercero % 2) == 0)
+                textBoxResultado.Text = "TU ULTIMO RESULTADO ES: \n" + "+" + segundo + "" + primero;
+            else
+                textBoxResultado.Text = "TU ULTIMO RESULTADO ES: \n" + "-" + segundo + "" + primero;
+
+        }
+        public void animacionGeneral()
+        {
+
+            int cont = 0;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                Thread.Sleep(i + 100);
+                if (cont == 0)
+                {
+                    if(i <45)
+                        labelPrimero.Text = "0";
+                    if (i < 75)
+                        labelSegundo.Text = "0";
+                    if (i < 100)
+                        labelSigno.Text = "+";
+                    cont++;
+                }
+                else if (cont == 1)
+                {
+                    if (i < 45)
+                        labelPrimero.Text = "1";
+                    if (i < 75)
+                        labelSegundo.Text = "1";
+                    if (i < 100)
+                        labelSigno.Text = "-";
+                    cont++;
+                }
+                else if (cont == 2)
+                {
+                    if (i < 45)
+                        labelPrimero.Text = "2";
+                    if (i < 75)
+                        labelSegundo.Text = "0";
+                    if (i < 100)
+                        labelSigno.Text = "+";
+                    cont++;
+                }
+                else if (cont == 3)
+                {
+                    if (i < 45)
+                        labelPrimero.Text = "3";
+                    if (i < 75)
+                        labelSegundo.Text = "1";
+                    if (i < 100)
+                        labelSigno.Text = "-";
+                    cont++;
+                }
+                else if (cont == 4)
+                {
+                    if (i < 45)
+                        labelPrimero.Text = "4";
+                    if (i < 75)
+                        labelSegundo.Text = "0";
+                    if (i < 100)
+                        labelSigno.Text = "+";
+                    cont++;
+                }
+                else if (cont == 5)
+                {
+                    if (i < 45)
+                        labelPrimero.Text = "5";
+                    if (i < 75)
+                        labelSegundo.Text = "1";
+                    if (i < 100)
+                        labelSigno.Text = "-";
+                    cont++;
+                }
+                else if (cont == 6)
+                {
+                    if (i < 45)
+                        labelPrimero.Text = "6";
+                    if (i < 75)
+                        labelSegundo.Text = "0";
+                    if (i < 100)
+                        labelSigno.Text = "+";
+                    cont++;
+                }
+                else if (cont == 7)
+                {
+                    if (i < 45)
+                        labelPrimero.Text = "7";
+                    if (i < 75)
+                        labelSegundo.Text = "1";
+                    if (i < 100)
+                        labelSigno.Text = "-";
+                    cont++;
+                }
+                else if (cont == 8)
+                {
+                    if (i < 45)
+                        labelPrimero.Text = "8";
+                    if (i < 75)
+                        labelSegundo.Text = "0";
+                    if (i < 100)
+                        labelSigno.Text = "+";
+                    cont++;
+                }
+                else if (cont == 9)
+                {
+                    if (i < 45)
+                        labelPrimero.Text = "9";
+                    if (i < 75)
+                        labelSegundo.Text = "1";
+                    if (i < 100)
+                        labelSigno.Text = "-";
+                    cont = 0;
+                }
+                labelPrimero.Refresh();
+                labelSegundo.Refresh();
+                labelSigno.Refresh();
+                if (i == 45)
+                {
+                    labelPrimero.Text = primero.ToString();
+                    labelPrimero.Refresh();
+                }
+                else if (i == 75) 
+                {
+                    labelSegundo.Text = segundo.ToString();
+                    labelSegundo.Refresh();
+                }
+                else if (i == 100)
+                {
+                    if ((tercero % 2) == 0)
+                        labelSigno.Text = "+";
+                    else
+                        labelSigno.Text = "-";
+                    labelSigno.Refresh();
+                }
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(i + 100);
+            }
+        }
+    }
+}
